4d620606117b54cac45b894a61ebc1af31a70cdf
Anand Ramdhani <anandkr.github@gmail.com>
2020-02-22 14:07:34 +0200

    Updates

---
+++ precache-manifest.7f2c2abbf23ca900ac9f1142fe5199a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ae91fd5a7b1f66333d2f8dece74a3b",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "aa40b184e347401c00c2",
+    "url": "/todo-app/static/css/main.6ce56e50.chunk.css"
+  },
+  {
+    "revision": "4f87ebcbf9dbecae999f",
+    "url": "/todo-app/static/js/2.95c28da9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/todo-app/static/js/2.95c28da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa40b184e347401c00c2",
+    "url": "/todo-app/static/js/main.f987e597.chunk.js"
+  },
+  {
+    "revision": "4d6efa66b0e8d73d3406",
+    "url": "/todo-app/static/js/runtime-main.1f4d1460.js"
+  }
+]);
